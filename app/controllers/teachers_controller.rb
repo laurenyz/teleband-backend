70be484c45e0798b721dde68d34ebf931afad1db
github username <github email address>
2020-07-28 11:49:00 -0400

    added teacher update grades method in teacher controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -31,11 +31,32 @@ class TeachersController < ApplicationController
             studentData: teacher.allStudentsData
         }
     end
-
-    def update
-
     
+    def updategrades
+
+        token = request.headers["Authentication"]
+        payload = decode(token)
+        teacher = Teacher.find(payload["teacher_id"])
+
+        assignments = params["updateAssignment"]
+        assignments.each do |id, score|
+            current = StudentAssignment.find(id)
+            current.tone = score["tone"]
+            current.expression = score["expression"]
+            current.rhythm = score["rhythm"]
+            current.submitted = true
+            current.save
+        end
+
+        render json: {
+            teacher: teacher,
+            studentData: teacher.allStudentsData
+        }
+
+
     end
+
+   
 #     {
 #   [
 #     {
