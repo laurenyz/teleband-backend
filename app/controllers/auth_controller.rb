0296122b3e3358b1e89b365762be7c62e96d84be
github username <github email address>
2020-08-10 13:26:53 -0400

    fixed login issue for student

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,10 +3,18 @@ class AuthController < ApplicationController
     def create 
         if params["school_id"]
             student = Student.find_by(school_id: params["school_id"])
-            render json: {
-                student: student,
-                token: params["school_id"]
-        }
+            
+            render json: student.to_json(
+            :except => [:created_at, :updated_at],
+            :include => [:student_assignments => 
+            {:except => 
+            [:created_at, :updated_at, :audios],
+            :include => 
+            [:assignment => 
+            {:except =>[ :created_at, :updated_at] 
+            }]}]
+        )
+        
         else
             teacher = Teacher.find_by(email: params["email"])
             if teacher && teacher.authenticate(params["password"])
