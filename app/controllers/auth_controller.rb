55d15842bb9bd915aa1c697f59767cef878dd1bc
github username <github email address>
2020-08-30 15:55:01 -0400

    Fixed input erros8_30_2020

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,17 +4,23 @@ class AuthController < ApplicationController
         if params["school_id"]
             student = Student.find_by(school_id: params["school_id"])
             
-            render json: student.to_json(
-            :except => [:created_at, :updated_at],
-            :include => [:student_assignments => 
-            {:except => 
-            [:created_at, :updated_at, :audios],
-            :include => 
-            [:assignment => 
-            {:except =>[ :created_at, :updated_at] 
-            }]}]
-        )
-        
+            if student
+                render json: student.to_json(
+                    :except => [:created_at, :updated_at],
+                    :include => [:student_assignments => 
+                    {:except => 
+                    [:created_at, :updated_at, :audios],
+                    :include => 
+                    [:assignment => 
+                    {:except =>[ :created_at, :updated_at] 
+                    }]}]
+                )
+            else
+                render json: {
+                error: true,
+                message: "Student does not exist"
+            }
+            end
         else
             teacher = Teacher.find_by(email: params["email"])
             if teacher && teacher.authenticate(params["password"])
