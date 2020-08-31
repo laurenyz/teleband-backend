c7f6ac6b235601d7dca057cbcfdcb43db6e906cd
laurenyz <laurenkyu@gmail.com>
2020-08-31 01:08:26 -0400

    adjusted attach_recording method. Need to fix aws access

--- app/controllers/student_assignments_controller.rb
+++ app/controllers/student_assignments_controller.rb
@@ -8,6 +8,7 @@ class StudentAssignmentsController < ApplicationController
     def show
         student_assignment = StudentAssignment.find(params[:id])
         assignment = student_assignment.assignment
+        student = student_assignment.student
          if student_assignment.student_recording.attached?
              student_audio = url_for(student_assignment.student_recording)
          else
@@ -16,7 +17,9 @@ class StudentAssignmentsController < ApplicationController
         
         render json: {
             title: assignment.title,
+            student: student.name,
             pdf_file: assignment.excerpts,
+            submitted: student_assignment.submitted,
             assignment_audio_recordings: {
                 assignment.title => assignment.audios
             },
@@ -27,11 +30,19 @@ class StudentAssignmentsController < ApplicationController
     def attach_recording
         student = Student.find_by(school_id: params[:school_id])
         student_assignment = StudentAssignment.find_by(student_id: student.id, assignment_id: params[:assignment_id])
-        r = student_assignment.student_recording.attach(params[:student_recording])
-        url = rails_blob_url(r.first, only_path: true)
-        render json: {
-                message: "Attached to File", 
-                url: url
-        }
+        student_assignment.student_recording.attach(params[:student_recording])
+        student_assignment.update(submitted: true, student_audio: url_for(student_assignment.student_recording))
+        if student_assignment.student_recording.attached?
+            render json: {
+                    message: "Attached to File", 
+                    url: student_assignment.student_audio,
+                    error: false
+            }
+        else 
+            render json: {
+                error: true,
+                message: "Audio could not upload, please try again."
+            }
+        end
     end
 end
