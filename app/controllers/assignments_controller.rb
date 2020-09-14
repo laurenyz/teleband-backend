9b0e987e6c3294786950b5971d4035213c6c8329
laurenyz <laurenkyu@gmail.com>
2020-09-13 20:56:13 -0400

    added basic create assignment method

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -8,4 +8,21 @@ class AssignmentsController < ApplicationController
         assignment = Assignment.find(params[:id])
         render json: assignment
     end
+
+    def create
+        assignment = Assignment.new(title: params[:title], excerpts:"", audios:"")  
+        if assignment.valid?
+            assignment.save
+            students = Student.all
+            students.each do |student|
+                StudentAssignment.create(student: student, assignment: assignment, student_audio:"", tone: nil, expression: nil, rhythm: nil)
+            end
+            render json: assignment
+        else
+            render json: {
+                error: true,
+                message: assignment.errors.full_messages
+            }
+        end
+    end
 end
