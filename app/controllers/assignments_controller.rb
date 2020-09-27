ada5afcec767de6ca5740994670b8b0a5e80c0a1
laurenyz <laurenkyu@gmail.com>
2020-09-27 17:01:59 -0400

    successfully attached notationPdf

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -12,14 +12,20 @@ class AssignmentsController < ApplicationController
     end
 
     def create
-        assignment = Assignment.new(title: params[:title], instructions: params[:instructions], notation_url: params[:notation_url], assignment_type: params[:formType]) 
+        assignment = Assignment.new(title: params[:title], instructions: params[:instructions], assignment_type:"audio", notation_url:"") 
         if assignment.valid?
             assignment.save
+            assignment.notationPdf.attach(params[:notationPdf])
+            assignment.update(notation_url: url_for(assignment.notationPdf))
             students = Student.all
             students.each do |student|
                 StudentAssignment.create(student: student, assignment: assignment, student_audio:"", tone: nil, expression: nil, rhythm: nil)
             end
-            render json: assignment
+            render json: {
+                    message: "Attached to File", 
+                    assignment: assignment,
+                    error: false
+            }
         else
             render json: {
                 error: true,
