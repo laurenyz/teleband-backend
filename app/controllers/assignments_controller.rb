f57996d632617b55beaa071017d50e900b6c3080
laurenyz <laurenkyu@gmail.com>
2020-09-28 22:45:36 -0400

    completed edit functionality

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -28,7 +28,38 @@ class AssignmentsController < ApplicationController
                 StudentAssignment.create(student: student, assignment: assignment, student_audio:"", tone: nil, expression: nil, rhythm: nil)
             end
             render json: {
-                    message: "Attached to File", 
+                    message: "Assignment Successfully Created", 
+                    assignment: assignment,
+                    error: false
+            }
+        else
+            render json: {
+                error: true,
+                message: assignment.errors.full_messages
+            }
+        end
+    end
+
+    def update
+        assignment = Assignment.find(params[:id])
+        assignment.update(title: params[:title], instructions: params[:instructions], category: params[:formType])
+        if params[:pdf] != "null"
+        assignment.pdf.attach(params[:pdf])
+        assignment.update(pdf_url: url_for(assignment.pdf))
+        end
+        if assignment.category != 'response'
+            if params[:accompaniment] != "null"
+            assignment.accompaniment.attach(params[:accompaniment])
+            assignment.update(accompaniment_url: url_for(assignment.accompaniment))
+            end
+            if params[:playing_sample] != "null"
+            assignment.playing_sample.attach(params[:playing_sample])
+            assignment.update(playing_sample_url: url_for(assignment.playing_sample))
+            end
+        end
+        if assignment.valid?
+            render json: {
+                    message: "Assignment Successfully Updated", 
                     assignment: assignment,
                     error: false
             }
